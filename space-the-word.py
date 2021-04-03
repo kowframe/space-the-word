0b915257016e1da3443183bb776f7290aaa4217c
kowframe <kowframe@gmail.com>
2021-04-04 00:38:21 +0700

    v1

---
+++ space-the-word.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Develop by: kowframe
+def is_stop(usr_cmd):
+    if usr_cmd == "exit" or usr_cmd == "EXIT":
+        return True
+
+while(True):
+    word = input("Tell something (type \"exit\" to exit): ")
+    if is_stop(word) is True:
+        break
+
+    text = ""
+    vowel = ["ิ", "ุ", "ี", "ุ", "ู", "ึ", "ื", "ั", "่", "้", "๊", "๋", "์"]
+
+    for a in word:
+        if(a in vowel):
+            text = text[:-1]
+        text += a + " "
+
+    print("\n*************")
+    print(text)
+    print("\n*************")
